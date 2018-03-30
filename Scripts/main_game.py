17e06e81d4d6abca6dd72631b53dcc8f5652a914
xsangyhix <marcin.lew.slon@gmail.com>
2018-03-30 20:58:39 +0200

    main game fixes

--- Scripts/main_game.py
+++ Scripts/main_game.py
@@ -5,19 +5,19 @@ import Scripts.player as player
 
 screen = scrn.Screen()
 
-arena_size = input('Please enter the size of the play arena: ')
-win_condition = input('Please enter the required sequence length to win: ')
+arena_size = int(input('Please enter the size of the play arena: '))
+win_condition = int(input('Please enter the required sequence length to win: '))
 play_arena = arena.Arena(arena_size, win_condition)
 win = False
-
-screen.update_arena(play_arena.get_table())
-screen.render()
 
 p1 = player.Player(input('Player 1 please enter your name: '), 1)
 p2 = player.Player(input('Player 2 please enter your name: '), 2)
 
 # p1 = player.Player('p1', 1)
 # p2 = player.Player('p2', 2)
+
+screen.update_arena(play_arena.get_table())
+screen.render()
 
 next_player = p1
 
@@ -26,7 +26,10 @@ while not win:
     mx = int(input('Column: ')) - 1
     my = int(input('Row: ')) - 1
 
-    play_arena.put_sign(next_player.number, [my, mx])
+    while play_arena.put_sign(next_player.number, [my, mx]) == 0:
+        mx = int(input('Column: ')) - 1
+        my = int(input('Row: ')) - 1
+
     screen.update_arena(play_arena.get_table())
     screen.render()
 
