c87e3f44d83992cb33f4d4a55a561c05129dac4a
xsangyhix <marcin.lew.slon@gmail.com>
2018-03-30 19:12:54 +0200

    First working prototype

--- Scripts/main_game.py
+++ Scripts/main_game.py
@@ -1,19 +1,44 @@
 import Scripts.screen as scrn
 import Scripts.arena as arena
+import Scripts.player as player
 
 
 screen = scrn.Screen()
-play_arena = arena.Arena(5, 3)
+
+arena_size = input('Please enter the size of the play arena: ')
+win_condition = input('Please enter the required sequence length to win: ')
+play_arena = arena.Arena(arena_size, win_condition)
+win = False
+
+screen.update_arena(play_arena.get_table())
+screen.render()
+
+p1 = player.Player(input('Player 1 please enter your name: '), 1)
+p2 = player.Player(input('Player 2 please enter your name: '), 2)
+
+# p1 = player.Player('p1', 1)
+# p2 = player.Player('p2', 2)
+
+next_player = p1
+
+while not win:
+    print('Player ' + next_player.name + ' please enter your move:')
+    mx = int(input('Column: ')) - 1
+    my = int(input('Row: ')) - 1
+
+    play_arena.put_sign(next_player.number, [my, mx])
+    screen.update_arena(play_arena.get_table())
+    screen.render()
+
+    if play_arena.check_for_win() == 1:
+        win = True
+        print('Player ' + next_player.name + ' won the game!')
+
+    if next_player is p1:
+        next_player = p2
+    else:
+        next_player = p1
 
 # screen.update_arena(play_arena.get_table())
 # screen.render()
 
-play_arena.put_sign(1, [1, 0])
-play_arena.put_sign(1, [2, 0])
-play_arena.put_sign(1, [1, 1])
-play_arena.put_sign(1, [1, 2])
-
-print(play_arena.check_for_win())
-
-screen.update_arena(play_arena.get_table())
-screen.render()
--- Scripts/screen.py
+++ Scripts/screen.py
@@ -3,8 +3,19 @@ class Screen(object):
         self.table_play_arena = []
 
     def render(self):
+        print('\n\n\n')
         for i in self.table_play_arena:
-            print(i)
+            line = '| '
+            for item in i:
+                if item == 0:
+                    line += ' - '
+                elif item == 1:
+                    line += ' X '
+                elif item == 2:
+                    line += ' O '
+            line += ' |'
+            print(line)
+        print('\n')
 
     def update_arena(self, arena):
         self.table_play_arena = arena
